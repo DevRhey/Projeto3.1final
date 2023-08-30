4a9961f9ccab68ffe26d6477ee086e62b40355f1
DevRhey <DevRhey@gmail.com>
2023-08-30 13:15:58 -0300

    Final

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $("#telefone").mask("(00)00000-0000", { placeholder: "(00) 0000-0000" });
+
+  $("form").validate({
+    rules: {
+      nome: { required: true },
+      email: { required: true, email: true },
+      mensagem: { required: true },
+    },
+    messages: {
+      nome: "Campo obrigatório",
+      email: "Campo obrigatório",
+      cpf: "Campo obrigatório",
+    },
+    submitHandler: function (form) {
+      alert("Menssagem enviada");
+    },
+    invalidHandler: function (event, validator) {
+      let camposIncorretos = validator.numberOfInvalids();
+      if (camposIncorretos) {
+        alert("Existem campos incorretos");
+      }
+    },
+  });
+});
